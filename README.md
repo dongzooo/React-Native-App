ec3b7ba9f027e87dbe5c509bef07076dcac8e0cb
dongzooo <dhel48@nate.com>
2022-06-27 09:34:44 +0900

    add login function

---
+++ README.md
@@ -0,0 +1,2 @@
+# React-Native-App
+리엑트 네이티브 앱 구축 연습
---
+++ ServerUse/app/src/main/java/com/adamsoft/serveruse/ItemRegisterActivity.java
@@ -0,0 +1,15 @@
+package com.adamsoft.serveruse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_register);
+        this.setTitle("아이템 추가");
+    }
+}
